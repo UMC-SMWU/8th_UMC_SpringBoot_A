54b8c92957142981fa61a0fdcafee192cb7b8fe1
yoonseo8167 <yoonseo8167@sookmyung.ac.kr>
2025-05-08 15:53:23 +0900

    chapter6-Q클래스 설정

---
+++ src/main/generated/umc8/spring/domain/QFoodCategory.java
@@ -0,0 +1,47 @@
+package umc8.spring.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QFoodCategory is a Querydsl query type for FoodCategory
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QFoodCategory extends EntityPathBase<FoodCategory> {
+
+    private static final long serialVersionUID = 1214581990L;
+
+    public static final QFoodCategory foodCategory = new QFoodCategory("foodCategory");
+
+    public final umc8.spring.domain.common.QBaseEntity _super = new umc8.spring.domain.common.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public QFoodCategory(String variable) {
+        super(FoodCategory.class, forVariable(variable));
+    }
+
+    public QFoodCategory(Path<? extends FoodCategory> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QFoodCategory(PathMetadata metadata) {
+        super(FoodCategory.class, metadata);
+    }
+
+}
+
