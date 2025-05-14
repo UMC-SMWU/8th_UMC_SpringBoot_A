615f3e89416e793a7714913c05cd8946ef69d9a6
happine2s <happine2s@sookmyung.ac.kr>
2025-05-14 11:20:42 +0900

    [mission7] API 응답 통일

---
+++ src/main/java/umc/spring/converter/TempConverter.java
@@ -0,0 +1,12 @@
+package umc.spring.converter;
+
+import umc.spring.web.dto.TempResponse;
+
+public class TempConverter {
+
+    public static TempResponse.TempTestDto toTempTestDTO(){
+        return TempResponse.TempTestDto.builder()
+                .testString("This is Test!")
+                .build();
+    }
+}
